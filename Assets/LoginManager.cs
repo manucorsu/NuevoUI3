aabc84fa414e6068b1d35edda8d5221abc4cc214
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-05-27 15:47:22 -0300

    eac

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections;
+﻿/*CONSIGNA:
+ * Cuando el usuario presione el BtnAceptar,
+ * debe escribirse un mensaje en la consola
+ * si el texto del InputPassword coincide
+ * con una contraseña predefinida por mi,
+ * escribir en la consola "ACCESS GRANTED",
+ * si no escribir "ACCESS DENIED"*/
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField inputPassword;
+    string password = "2705";
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +26,17 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CheckPwd()
+    {
+        if(inputPassword.text == password)
+        {
+            Debug.Log("ACCESS GRANTED");
+        }
+        else
+        {
+            Debug.Log("ACCESS DENIED");
+        }
     }
 }
